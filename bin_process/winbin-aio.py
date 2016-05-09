d0979310167183f4c645e129cab99df4691febb0
Viktor Polak <viktor.polak@canterbury.ac.nz>
2016-05-09 12:56:05 +1200

    minor cleanup

--- bin_process/winbin-aio.py
+++ bin_process/winbin-aio.py
@@ -14,7 +14,6 @@ ISSUES: think of better filename
 """
 
 import os
-import os.path
 import sys
 sys.path.append(os.path.abspath(os.path.curdir))
 import shutil
@@ -27,8 +26,13 @@ if sys.argv[1] == 'test_mode':
     print('Running under test mode.')
     from postprocess_test.test_params import *
 
+fdbin2wcc_bin = os.path.join(wcc_prog_dir, 'fdbin2wcc')
+
+verify_binaries([fdbin2wcc_bin])
 verify_files([FD_STATLIST])
-verify_user_dirs([vel_dir])
+verify_logfiles([FILELIST])
+verify_strings([output_prefix, scale, TSTRT])
+verify_user_dirs([vel_dir, bin_output])
 
 STATS, LATS, LONS = get_stations(FD_STATLIST, True)
 COMPS = ['080', '170', 'ver']
@@ -45,7 +49,7 @@ list_handle = open(FILELIST, 'w')
 
 filepattern = os.path.join(bin_output, output_prefix+ '_seis*.e3d')
 print filepattern
-list_of_files = '\n'.join([file_path for file_path in glob(filepattern)])+'\n'
+list_of_files = '\n'.join([file_path for file_path in glob(filepattern)]) + '\n'
 print list_of_files
 list_handle.write(list_of_files)
 list_handle.close()
@@ -56,8 +60,8 @@ for s_index, stat in enumerate(STATS):
     statfile = os.path.join(vel_dir, stat)
 
     for c_index, comp in enumerate(COMPS):
-        cmd = [os.path.join(wcc_prog_dir,'fdbin2wcc'), 'all_in_one=1', 'filelist=' + FILELIST,
-                'ix=' + IX[c_index], 'scale=' + SCALE, 'flip=' + FLIP[c_index], 'tst=' + TSTRT,
+        cmd = [fdbin2wcc_bin, 'all_in_one=1', 'filelist=' + FILELIST,
+                'ix=' + IX[c_index], 'scale=' + scale, 'flip=' + FLIP[c_index], 'tst=' + TSTRT,
                 'stat=' + stat, 'comp=' + comp, 'outfile=' + statfile + '.' + comp,
                 'nseis_comps=9', 'swap_bytes=0', 'tst2zero=0', 'outbin=0'] 
         print ' '.join(cmd)
