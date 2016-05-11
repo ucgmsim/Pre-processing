11a411e63b5688eca950ab87e44849ac14585970
Sung Bae <sung.bae@nesi.org.nz>
2016-05-11 23:37:25 +0000

    Renaming 20150511 to 20160511

---
+++ bin_process/20160511/gen_ts.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python2
+"""
+Converts OutBin/*_xyts.e3d files to TSlice/TSFiles/*_ts0000.0 which can then be plotted.
+
+@author Viktor Polak, Sung Bae
+@date 8 April 2016
+
+Replaces gen_ts.csh. Converted to Python, uses globally set variables.
+
+USAGE: execute from current directory. Input and output basenames are set in params.py.
+
+ISSUES: 
+"""
+
+from subprocess import call
+import sys
+import os.path
+sys.path.append(os.path.abspath(os.path.curdir))
+from shared import *
+from params import *
+
+verify_files([ts_file, GRIDFILE])
+verify_strings([ts_start, ts_total, ts_inc, ts_out_prefix, swap_bytes, scale, \
+        dx_ts, dy_ts, dz_ts, ABSMAX, lonlat_out])
+verify_user_dirs([ts_out_dir])
+
+for tscnt in range(ts_start, ts_total):
+    tsnum = str(int(tscnt * ts_inc))
+
+    outf = ts_out_prefix + '_ts' + str(tscnt).zfill(4)
+    print(outf)
+
+    cmd = [os.path.join(wcc_prog_dir, 'ts2xyz'), 'infile=' + ts_file, 'outfile=' + outf, \
+            'swap_bytes=' + swap_bytes, 'gridfile=' + GRIDFILE, 'xyts=1', 'scale=' + scale, \
+            'ts=' + tsnum, 'trv=0', 'dxts=' + dx_ts, 'dyts=' + dy_ts, 'dzts=' + dz_ts, \
+            'absmax=' + ABSMAX, 'read_header=1', 'outbin=1', 'lonlat=' + lonlat_out, 'geoproj=1']
+    print ' '.join(cmd)
+    call(cmd)
---
+++ bin_process/20160511/recipes/2010Sep4_v1_Cantv1_64.100_v3.04/plot_ts.ll
@@ -0,0 +1,32 @@
+# Example MPI LoadLeveler Job file
+# @ shell = /bin/bash
+#
+# @ job_name = plot_ts
+#
+# @ job_type = serial 
+#
+# @ wall_clock_limit = 04:00:00
+#
+# Groups to select from: UC, UC_merit, NZ, NZ_merit
+# @ group = NZ_merit 
+# Your project number, either bfcs or nesi, followed by 5 digits
+# @ account_no = nesi00213
+# @ output = $(job_name).$(schedd_host).$(jobid).out
+# @ error = $(job_name).$(schedd_host).$(jobid).err
+# To receive an email when yourjob has completed:
+# @ notification       = never
+# @ notify_user        = myemail@gmail.com
+ 
+# @ class = p7linux_serial
+#
+# To improve performance it is important to specify task and memory affinities
+# @ rset = rset_mcm_affinity
+# @ environment = COPY_ALL
+# @ queue
+  
+# suggested environment settings:
+export MP_EAGER_LIMIT=65536
+export MP_SHARED_MEMORY=yes
+export MEMORY_AFFINITY=MCM
+ 
+csh plot_ts_beatrice.csh
