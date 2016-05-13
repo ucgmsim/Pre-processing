7b3296660c8d221f09ab025a03303117c3ed9ef5
Viktor Polak <viktor.polak@canterbury.ac.nz>
2016-05-13 15:17:08 +1200

    hf sim outputs to STDIO instead of file

--- bin_process/20160511/hfsims-stats.py
+++ bin_process/20160511/hfsims-stats.py
@@ -46,10 +46,9 @@ with open(local_statfile, 'w') as lp:
 # some info echoed in original script, might be useful for some
 print("%d %s" % (n_stat, hf_v_model))
 
-with open('hf.out', 'w') as op:
-    # execute calculation, stdout and errout to hf.out file, stdin from pipe
-    hf_pipe = Popen([hf_sim_bin], stdin = PIPE, stdout = op, stderr = op)
-    # pipe input data into binary stdin
-    # look at hf_sim_bin source code (same dir) for more info on parameters
-    hf_pipe.communicate('\n%(hf_sdrop)s\n%(local_statfile)s\n%(out_prefix)s\n%(hf_rayset)s\n%(hf_site_amp)s\n4   0  0.02  19.9\n%(hf_seed)s\n%(n_stat)s\n%(hf_t_len)s %(hf_dt)s %(hf_fmax)s %(hf_kappa)s %(hf_qfexp)s\n%(hf_rvfac)s %(hf_shal_rvfac)s %(hf_deep_rvfac)s %(hf_czero)s %(hf_calpha)s\n%(hf_mom)s %(hf_rupv)s\n%(hf_slip)s\n%(hf_v_model)s\n%(hf_vs_moho)s\n-99 0.0 0.0 0.0 0.0 1\n-1\n%(hf_fa_sig_1)s 0.0 %(hf_rv_sig_1)s\n1\n' % locals())
+# execute calculation, stdin from pipe
+hf_pipe = Popen([hf_sim_bin], stdin = PIPE)
+# pipe input data into binary stdin
+# look at hf_sim_bin source code (same dir) for more info on parameters
+hf_pipe.communicate('\n%(hf_sdrop)s\n%(local_statfile)s\n%(out_prefix)s\n%(hf_rayset)s\n%(hf_site_amp)s\n4   0  0.02  19.9\n%(hf_seed)s\n%(n_stat)s\n%(hf_t_len)s %(hf_dt)s %(hf_fmax)s %(hf_kappa)s %(hf_qfexp)s\n%(hf_rvfac)s %(hf_shal_rvfac)s %(hf_deep_rvfac)s %(hf_czero)s %(hf_calpha)s\n%(hf_mom)s %(hf_rupv)s\n%(hf_slip)s\n%(hf_v_model)s\n%(hf_vs_moho)s\n-99 0.0 0.0 0.0 0.0 1\n-1\n%(hf_fa_sig_1)s 0.0 %(hf_rv_sig_1)s\n1\n' % locals())
 
