a94bb6cd7edd992d7017d983c3741d9c66619f7d
Viktor Polak <viktor.polak@canterbury.ac.nz>
2016-05-27 14:06:16 +1200

    Scale movie to fit in XVGA (most projector's resolution) box.

--- bin_process/devel/make_movie.sh
+++ bin_process/devel/make_movie.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 source e3d.par
 ffbuild=/nesi/home/vap30/bin/$(uname -s)/ffmpeg
+if [ "$(uname -s)" == "Linux" ]; then
+    echo Warning: runing on linux VM, performance will be affected.
+    echo Running directly on AIX node is recommended.
+fi
 
 # https://trac.ffmpeg.org/wiki/Create%20a%20video%20slideshow%20from%20images
 
@@ -23,12 +27,12 @@ fps=$(echo "1/($dt * $dtts) * $speed" | bc)
 # -i: input filename format, alternative is using glob
 # -c:v: video codec, list options: 'ffmpeg -codecs | grep EV'
 # -r: output framerate (keep same as input to prevent duplicated frames)
+# -y: overwrite existing output without asking
 echo
 echo Movie creation starts
 echo
-# ffmpeg would wait for user input (stderr) to ask y/N overwrite
-if [ -e "$name.mov" ]; then rm "$name.mov"; fi
-ffbuild -framerate $fps -i $plot_png_dir/ts-str%04d.png \
+ffbuild -y -framerate $fps -i $plot_png_dir/ts-str%04d.png \
+        -vf scale="'if(gt(a,1024/768),1024,-1)':'if(gt(a,1024/768),-1,768)'" \
         -c:v qtrle -r $fps $name.mov 2>/dev/null
 echo Movie creation ends
 
