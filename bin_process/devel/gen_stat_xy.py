04a01efb13bb9893a3fc58aee955db0382acf106
Viktor Polak <viktor.polak@canterbury.ac.nz>
2016-08-23 11:55:46 +1200

    gen_stat_xy - can't remember what

--- bin_process/devel/gen_stat_xy.py
+++ bin_process/devel/gen_stat_xy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
-from math import sin, cos, radians
+from math import sin, cos
+import os
 import sys
 
 import numpy as np
@@ -8,48 +9,72 @@ import numpy as np
 from params import *
 from tools import *
 
-# have to know which point the station is at
-lat = -43.5928
-lon = 172.3811
+gmt_seis = 'gmt-seismo.xy'
 
-try:
-    x, y = ll2gp(lat, lon, float(MODEL_LAT), float(MODEL_LON), \
-            float(MODEL_ROT), int(nx), int(ny), float(hh), \
-            int(dx_ts), int(dy_ts), decimated = True)
-except InputError:
-    print('Station coordinates outside simulation domain')
-    raise
-# position within timeslice file which this x, y appears
-pos = y * int(nx) / int(dy_ts) + x
+if os.path.exists(gmt_seis):
+    os.remove(gmt_seis)
 
 # seismo data in timeslices, these have to be present for plotting anyway
 # reduces dependencies of files, can plot stations not in statfile
 ts = [ts_out_prefix + '_ts%.4d.0' % (i) \
         for i in xrange(int(ts_total))]
-ds = [np.memmap(filename = ts[i], dtype = '3>f', mode = 'r')[pos][-1] \
-        for i in xrange(int(ts_total))]
-v_max = max(np.abs(ds))
-
 
 ###
-### GENERATE XY
+### read values, calculate maximums
 ###
-xfac = 35./len(ds)
-yfac = 15./v_max
-lat0 = lat + 0.01
-lon0 = lon + 0.01
-xazim = 90
-if xazim % 360 - 180 < 0:
-    yazim = xazim - 90
-else:
-    yazim = xazim + 90
-lls = ['%f %f\n' % (lon0, lat0)]
-for i, value in enumerate(ds):
-    dy = value * yfac
-    dx = i * xfac
-    lat1, lon1 = ll_shift(lat0, lon0, dx, xazim)
-    lat1, lon1 = ll_shift(lat1, lon1, dy, yazim)
-    lls.append('%f %f\n' % (lon1, lat1))
+all_ds = []
+max_ds = 0
+for desc in plot_seismo:
+    lat, lon, offset, oazim, xazim, tlen, yamp = desc
 
-with open('gmt-seismo.xy', 'w') as sp:
-    sp.write(''.join(lls))
+    try:
+        x, y = ll2gp(lat, lon, float(MODEL_LAT), float(MODEL_LON), \
+                float(MODEL_ROT), int(nx), int(ny), float(hh), \
+                int(dx_ts), int(dy_ts), decimated = True)
+    except InputError:
+        print('Station coordinates outside simulation domain')
+        raise
+    # position within timeslice file which this x, y appears
+    pos = y * int(nx) / int(dy_ts) + x
+
+    ds = [np.memmap(filename = ts[i], dtype = '3>f', mode = 'r')[pos][-1] \
+            for i in xrange(int(ts_total))]
+
+    all_ds.append(ds)
+    v_max = max(np.abs(ds))
+    max_ds = max(max_ds, v_max)
+
+# y scaling factor based on overall max (km)
+yfac = float(yamp)/max_ds
+
+###
+### generate XY data based on read values and max / other params
+###
+for i, desc in enumerate(plot_seismo):
+    lat, lon, offset, oazim, xazim, tlen, yamp = desc
+
+    # scaling factor to produce wanted x, y lengths (km)
+    xfac = float(tlen)/len(all_ds[i])
+
+    # offset of beginning of seismogram
+    lat0, lon0 = ll_shift(lat, lon, offset, oazim)
+
+    # rotate as wanted (do not visually flip seismo)
+    if xazim % 360 - 180 < 0:
+        yazim = xazim - 90
+    else:
+        yazim = xazim + 90
+
+    lls = ['%f %f\n' % (lon0, lat0)]
+    for i, value in enumerate(all_ds[i]):
+        dy = value * yfac
+        dx = i * xfac
+        # find next point using distance / bearing
+        lat1, lon1 = ll_shift(lat0, lon0, dx, xazim)
+        lat1, lon1 = ll_shift(lat1, lon1, dy, yazim)
+        lls.append('%f %f\n' % (lon1, lat1))
+
+    with open(gmt_seis, 'a') as sp:
+        sp.write('> station at %f %f\n' % (lon, lat))
+        sp.write(''.join(lls))
+
