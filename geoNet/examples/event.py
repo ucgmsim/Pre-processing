986db35cdbde202b6198a3a62683cd143da916c0
ahsan <ahsan@nomultiverse.canterbury.ac.nz>
2017-02-17 09:20:17 +1300

    cleanup 1

--- geoNet/examples/event.py
+++ geoNet/examples/event.py
@@ -27,8 +27,6 @@ def getData(loc, BASE_URL):
     """
     init_time=time()
     print("Downloading data ...")
-    #only the BASE_RUL needs to be changed in this example
-    #BASE_URL="ftp://ftp.geonet.org.nz/strong/processed/Proc/2017/02_Feb/2017-02-01_102129/Vol1/data/"
 
     sg.make_dataPlot_dirs_v2(loc=loc,num_vol=1)
 
@@ -72,11 +70,6 @@ def event_statsll(fname, loc,
     std_out = open("event_statsll_stdout.txt", 'w')
     #Firs get statsll dictionary by assigning lon, lat from the saved list known 
     #to be in WGS84 coordinates
-    #fname="event_stats.ll"
-    ##loc_all_geoNet_stats="/nesi/projects/nesi00213/StationInfo"
-    #loc_all_geoNet_stats="."
-    #fname_all_geoNet_stats="all_geoNet_stats+2016-12-20.ll"
-    #loc_V1A="/".join([os.getcwd(), "Vol1", "data"])
     (
     event_stats1,
     fname_statsll
@@ -111,8 +104,7 @@ def event_statsll(fname, loc,
             (lon, lat) = event_stats1[stat_code]
             f.write("{:<15.4f} {:^15.4f} {:^10s} \n".format(lon, lat, stat_code))
 
-    #or use the convenience function
-    #write_statsll(loc, fname, event_stats1)
+    #write kml file to view with google earth/map
     write_stats_kml(loc, fname_statsll.split(".")[0]+".kml", event_stats1)
 
     std_out.close()
@@ -134,9 +126,6 @@ def processData(LOC):
     std_out = open("processData_stdout.txt", 'w')
     sys.stdout = std_out
 
-    #Only LOC needs to be changed in this example, LOC is where the geoNet data
-    #files are placed after download
-    #LOC="/".join([os.getcwd(), "Vol1", "data"])
 
     FILE_NAMES = []
     event_stats_V1A = glob("/".join([LOC,"*.V1A"]))
@@ -156,7 +145,8 @@ def processData(LOC):
                 continue
             
             #When appended zeroes at the beginning of the record are removed, the 
-            #record might then be empty, skipp processing in such a case
+            #record might then be empty, skipp processing in such a case. 
+            #The number 10 is rather arbirary, 1 should suffice
             agf=adjust_gf_for_time_delay(gf)
             if agf.comp_1st.acc.size <= 10: 
                 std_out.write("no elements in %s. Skipping it.\n" %station_file_name)
@@ -181,6 +171,7 @@ def processData(LOC):
 
         #extract stat code
         #stat_code = station_file_name.split(".")[0].split("_")[2]
+        #This could be placed in a try, except block
         YYYYMMDD, HHMM, stat_code = utils.parse_V1A_fname(station_file_name)
 
         try:
@@ -212,7 +203,6 @@ def plot_accvel(parent_dir_loc, plot_dir_accBB, plot_dir_velBB,
     init_time=time()
     print("Creating acc and vel plots for all Observed SMSs ...")
 
-    #import numpy as np
     import matplotlib
     # Force matplotlib to not use any Xwindows backend.
     matplotlib.use('Agg')
@@ -223,24 +213,12 @@ def plot_accvel(parent_dir_loc, plot_dir_accBB, plot_dir_velBB,
     from geoNet.utils import get_sorted_stats_code,  read_statsll, get_processed_stats_list
 
     init_time = time()
-    #parent_dir_loc="/nesi/projects/nesi00213/ObservedGroundMotions/ahsan/Mw4pt9_20110429_190804_11Jan2017/Vol1/data"
-    #parent_dir_loc="/nesi/projects/nesi00213/RealTime/Obs/Mw6pt6_2013-08-16_023105/Vol1/data"
-    #plot_dir_accBB="accBB"
-    #plot_dir_velBB="velBB"
-    #plot_dir_accLF_0pt25="accLF_0pt25"
-    #plot_dir_velLF_0pt25="velLF_0pt25"
-    #plot_dir="velBB"
     loc_accBB="/".join([parent_dir_loc, plot_dir_accBB])
     loc_velBB="/".join([parent_dir_loc, plot_dir_velBB])
-    #loc_accLF_0pt25="/".join([parent_dir_loc, plot_dir_accLF_0pt25])
-    #loc_velLF_0pt25="/".join([parent_dir_loc, plot_dir_velLF_0pt25])
 
     loc_acc = loc_accBB
     loc_vel = loc_velBB
 
-
-    #stats_dict = read_statsll("/nesi/projects/nesi00213/RealTime/Obs/Mw6pt6_2013-08-16_023105",
-    #                          'geonet_stations_20170117.ll')
     stats_dict = read_statsll(loc_statsll, fname_statsll)
     #Some of the SMSs may not be processed. Assign stats_dict to only those that were processed.
     stats_dict = get_processed_stats_list(loc_velBB, stats_dict, verbose=True)
@@ -250,7 +228,6 @@ def plot_accvel(parent_dir_loc, plot_dir_accBB, plot_dir_velBB,
     pdf_acc = PdfPages('plots_acc.pdf')
     pdf_vel = PdfPages('plots_vel.pdf')
     for stat in sorted_stats_code:
-        #stat = stat.strip("\n")
         stat_acc, fig_acc, ax_acc = get_stat_acc_plot(loc_acc, stat["name"])
         pdf_acc.savefig()
 
@@ -282,22 +259,11 @@ def plot_psa(parent_dir_loc, plot_dir_accBB, plot_dir_velBB,
     init_time=time()
     print("Creating pSA plots for all Observed SMSs ...")
 
-    #parent_dir_loc="/nesi/projects/nesi00213/ObservedGroundMotions/Mw5pt95_20150105_174841/Vol1/data"
-    #parent_dir_loc="/nesi/projects/nesi00213/RealTime/Obs/Mw6pt6_2013-08-16_023105/Vol1/data"
-    ##plot_dir="velLF"
-    #plot_dir_accBB="accBB"
-    #plot_dir_velBB="velBB"
-    #plot_dir_accLF_0pt25="accLF_0pt25"
-    #plot_dir="velBB"
     loc_accBB="/".join([parent_dir_loc, plot_dir_accBB])
     loc_velBB="/".join([parent_dir_loc, plot_dir_velBB])
-    #loc_accLF_0pt25="/".join([parent_dir_loc, plot_dir_accLF_0pt25])
 
     loc_acc = loc_accBB
 
-
-    #stats_dict = read_statsll("/nesi/projects/nesi00213/RealTime/Obs/Mw6pt6_2013-08-16_023105",
-    #                          'geonet_stations_20170117.ll')
     stats_dict = read_statsll(loc_statsll, fname_statsll)
     #Some of the SMSs may not be processed. Assign stats_dict to only those that were processed.
     stats_dict = get_processed_stats_list(loc_velBB, stats_dict, verbose=True)
@@ -428,7 +394,6 @@ if __name__ == "__main__":
     
     keyValue = keyValueFromTxt(args['fname']) 
 
-
     loc = keyValue['loc'] 
     BASE_URL = keyValue['BASE_URL']
     loc_statsll = keyValue['loc_statsll']
@@ -439,36 +404,15 @@ if __name__ == "__main__":
     obs_velDir = keyValue['obs_velDir']
     obs_accDir = keyValue['obs_accDir']
 
-
-    #loc = os.getcwd()
-    #BASE_URL="ftp://ftp.geonet.org.nz/strong/processed/Proc/2017/02_Feb/2017-02-01_102129/Vol1/data/"
-    #BASE_URL="ftp://ftp.geonet.org.nz/strong/processed/Proc/2017/02_Feb/2017-02-02_074142/Vol1/data/"
     getData(loc, BASE_URL)
-
-    #fname_statsll="event_stats.ll"
-    #loc_statsll=os.getcwd()
-    #loc_all_geoNet_stats="/nesi/projects/nesi00213/StationInfo"
-    #loc_all_geoNet_stats="."
-    #fname_all_geoNet_stats="all_geoNet_stats_2016-12-20.ll"
-    #loc_V1A="/".join([os.getcwd(), "Vol1", "data"])
 
     event_statsll(fname_statsll, loc_statsll,
                   loc_all_geoNet_stats, fname_all_geoNet_stats,
                   loc_V1A)
 
 
-    #LOC="/".join([os.getcwd(), "Vol1", "data"])
     processData(loc_V1A)
 
-#    plot_accvel(loc_V1A, "accBB", "velBB",
-#                loc_statsll, fname_statsll)
-#
-#    plot_psa(loc_V1A, "accBB", "velBB",
-#             loc_statsll, fname_statsll)
-#
-#    IMsOnMap(loc_V1A, "accBB", "velBB",
-#             loc_statsll, fname_statsll)
-#
     plot_accvel(loc_V1A, obs_accDir, obs_velDir,
                 loc_statsll, fname_statsll)
 
