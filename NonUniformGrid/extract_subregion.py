7c21b59ed91107726b07986ca2e9a04f86436d90
Daniel Lagrava <dlagrava@gmail.com>
2017-03-03 10:40:24 +1300

    We can read the coordinates from VeloModCorners file

--- NonUniformGrid/extract_subregion.py
+++ NonUniformGrid/extract_subregion.py
@@ -5,22 +5,42 @@ import csv
 import tools
 
 # load GeoJSON file containing sectors
-with open('test.json',mode='r') as f:
-    js = json.load(f)
+# with open('test.json',mode='r') as f:
+#     js = json.load(f)
+
+# load VeloModCorners.txt
+list_lon = []
+list_lat = []
+with open("VeloModCorners.txt") as corners:
+    lines = corners.readlines()
+    for line in lines:
+        try:
+            lon, lat = map(float, line.split("\t"))
+            print lon, lat
+            list_lon.append(lon)
+            list_lat.append(lat)
+        except ValueError:
+            print "Ignoring",line.strip()
+
+geo_json = {"type": "Feature",
+             "geometry": {
+                 "type": "Polygon",
+                 "coordinates" : [[[lon,lat] for lon,lat in zip(list_lon,list_lat)]]
+             }
+            }
+print geo_json
 
 point_list = []
-all_points = tools.read_csv(sys.argv[1],delimiter="\t")
+all_points = tools.read_csv(sys.argv[1], delimiter="\t")
 for point in all_points:
-    point_list.append(Point(float(point[0]),float(point[1])))
+    point_list.append(Point(float(point[0]), float(point[1])))
 
 print len(point_list)
 
-with open('cropped_'+sys.argv[1],mode="w") as out:
+with open('cropped_' + sys.argv[1], mode="w") as out:
     # check each polygon to see if it contains the point
     for point in point_list:
-        for feature in js['features']:
-            polygon = shape(feature['geometry'])
-            if polygon.contains(point):
-                out.write("%f %f\n" % (point.x, point.y))
+        polygon = shape(geo_json['geometry'])
 
-
+        if polygon.contains(point):
+           out.write("%f %f\n" % (point.x, point.y))
