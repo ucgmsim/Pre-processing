16319f099f282803f6e3eafa94d10916526ab7dc
Sung Eun Bae <sung.bae@canterbury.ac.nz>
2017-03-27 15:14:50 +1300

    Changes for realtime workflow automation

--- SrfGen/createSRF.py
+++ SrfGen/createSRF.py
@@ -8,13 +8,7 @@ import sys
 
 import numpy as np
 
-GSF_BIN = '/nesi/projects/nesi00213/tools/fault_seg2gsf'
-# note version is missing, will be appended
-FF_SRF_BIN = '/nesi/projects/nesi00213/tools/genslip'
-PS_SRF_BIN = '/nesi/projects/nesi00213/tools/generic_slip2srf'
-SRF_JOIN_BIN = '/nesi/projects/nesi00213/tools/srf_join'
-STOCH_BIN = '/nesi/projects/nesi00213/tools/srf2stoch'
-VELFILE = 'lp_generic1d-gp01_v1.vmod'
+from srf_config import *
 
 def srf_join(in1, in2, out):
     """
