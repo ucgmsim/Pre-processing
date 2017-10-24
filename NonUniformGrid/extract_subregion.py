62da0c22dd280154845e99a9c65411f9ebf7ce20
Daniel Lagrava <dlagrava@gmail.com>
2017-10-25 11:00:21 +1300

    Improved sub-region extraction to take a parameter file in the format lat, lon

--- NonUniformGrid/extract_subregion.py
+++ NonUniformGrid/extract_subregion.py
@@ -11,11 +11,12 @@ import tools
 # load VeloModCorners.txt
 list_lon = []
 list_lat = []
-with open("VeloModCorners.txt") as corners:
+poly_file = sys.argv[2]
+with open(poly_file) as corners:
     lines = corners.readlines()
     for line in lines:
         try:
-            lon, lat = map(float, line.split("\t"))
+            lat, lon = map(float, line.split(","))
             print lon, lat
             list_lon.append(lon)
             list_lat.append(lat)
@@ -31,18 +32,25 @@ geo_json = {"type": "Feature",
 print geo_json
 
 point_list = []
-all_points = tools.read_csv(sys.argv[1], delimiter="\t")
+#all_points = tools.read_csv(sys.argv[1], delimiter="\t")
+
+with open(sys.argv[1]) as f:
+    lines = f.readlines()
+    lines = map(lambda x: x.strip().replace("\t"," "), lines)
+    all_points = map(lambda x: x.split(" "), lines)
 
 for point in all_points:
+    point = filter(lambda x: x != "", point)
+    print point
     point_list.append([float(point[0]), float(point[1]), point[2]])
 
 
 print len(point_list)
 
-with open('cropped_' + sys.argv[1], mode="w") as out:
+with open('cropped_test.csv' , mode="w") as out:
     # check each polygon to see if it contains the point
     polygon = shape(geo_json['geometry'])
     for point in point_list:
         tmp_point = Point(point[0],point[1])
         if polygon.contains(tmp_point):
-           out.write("%f\t%f\t%s\n" % (point[0], point[1], point[2]))
+           out.write("%f %f %s\n" % (point[0], point[1], point[2]))
