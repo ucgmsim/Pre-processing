185397736735366b027d25f862aeaac3b1a8a3cd
Sung Bae <sung.bae@nesi.org.nz>
2018-05-30 02:23:09 +0000

    srf_config.py for kupe

--- SrfGen/srf_config.py
+++ SrfGen/srf_config.py
@@ -1,11 +1,20 @@
 import os
 script_dir = os.path.dirname(os.path.abspath(__file__))
-
-GSF_BIN='/nesi/projects/nesi00213/tools/fault_seg2gsf_dipdir'
-# note version is missing, will be appended
-FF_SRF_BIN='/nesi/projects/nesi00213/tools/genslip'
-PS_SRF_BIN='/nesi/projects/nesi00213/tools/generic_slip2srf'
-STOCH_BIN='/nesi/projects/nesi00213/tools/srf2stoch'
-# default 1d velocity model
 VELOCITY_MODEL='%s/lp_generic1d-gp01_v1.vmod' % (script_dir)
 
+######   hypocentre
+#GSF_BIN='/nesi/projects/nesi00213/tools/fault_seg2gsf_dipdir'
+## note version is missing, will be appended
+#FF_SRF_BIN='/nesi/projects/nesi00213/tools/genslip'
+#PS_SRF_BIN='/nesi/projects/nesi00213/tools/generic_slip2srf'
+#STOCH_BIN='/nesi/projects/nesi00213/tools/srf2stoch'
+## default 1d velocity model
+
+
+######  Kupe
+GSF_BIN='/nesi/transit/nesi00213/tools/fault_seg2gsf_dipdir'
+# note version is missing, will be appended
+FF_SRF_BIN='/nesi/transit/nesi00213/tools/genslip'
+PS_SRF_BIN='/nesi/transit/nesi00213/tools/generic_slip2srf'
+SRF_JOIN_BIN='/nesi/transit/nesi00213/tools/srf_join'
+STOCH_BIN='/nesi/transit/nesi00213/tools/srf2stoch'
