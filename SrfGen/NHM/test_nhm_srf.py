26526095e7b8d1866b38cc0012ea981024e4dda4
Viktor Polak <viktor.polak@canterbury.ac.nz>
2018-09-19 16:00:04 +1200

    test srf item 2 complete

--- SrfGen/NHM/test_nhm_srf.py
+++ SrfGen/NHM/test_nhm_srf.py
@@ -28,26 +28,58 @@ def test_mw_vs_area(srf_infos):
                 ss_area.append(area)
 
     # plot
-    fig = plt.figure(figsize = (16 / 3., 9 / 3.), dpi = 150)
-    min_x = 10
-    max_x = 0
+    fig = plt.figure(figsize = (8, 5), dpi = 150)
+    min_y = 10
+    max_y = 0
     if len(ds_mw):
-        min_x = min(min(ds_mw), min_x)
-        max_x = max(max(ds_mw), max_x)
+        min_y = min(min(ds_mw), min_y)
+        max_y = max(max(ds_mw), max_y)
     if len(ss_mw):
-        min_x = min(min(ss_mw), min_x)
-        max_x = max(max(ss_mw), max_x)
-    x = np.linspace(min_x, max_x, 100)
+        min_y = min(min(ss_mw), min_y)
+        max_y = max(max(ss_mw), max_y)
+    y = np.linspace(min_y, max_y, 100)
 
     if len(ds_mw):
-        plt.plot(x, 10**(x-4), label='Leonard (dip slip)')
-        plt.plot(ds_mw, ds_area, label='SRF (dip slip)', marker='x', linestyle='None')
+        plt.plot(10**(y-4), y, label='Leonard (dip slip)')
+        plt.plot(ds_area, ds_mw, label='SRF (dip slip)', marker='x', linestyle='None')
     if len(ss_mw):
-        plt.plot(x, 10**(x-3.99), label='Leonard (strike slip)')
-        plt.plot(ss_mw, ss_area, label='SRF (strike slip)', marker='x', linestyle='None')
+        plt.plot(10**(y-3.99), y, label='Leonard (strike slip)')
+        plt.plot(ss_area, ss_mw, label='SRF (strike slip)', marker='x', linestyle='None')
+
     plt.legend(loc='best')
+    plt.gca().set_xscale('log')
+    plt.title('Area vs Magnitude')
+    plt.ylabel('Magnitude')
+    plt.xlabel('Area (sq. km)')
+    plt.show()
+    plt.close()
 
-    plt.gca().set_yscale('log')
+def test_mw_vs_nrup(srf_dirs):
+    nrup = []
+    mw = []
+    for d in srf_dirs:
+        infos = glob(os.path.join(d, '*.info'))
+        nrup.append(len(infos))
+        with h5open(infos[0], 'r') as h:
+            mw.append(h.attrs['mag'])
+
+    # plot
+    x = []
+    x.append(min(mw))
+    x.append(max(mw))
+    if x[0] < 6 < x[-1]:
+        x.insert(1, 6)
+    if x[0] < 8 < x[-1]:
+        x.insert(-1, 8)
+
+    fig = plt.figure(figsize = (8, 5), dpi = 150)
+    plt.plot(x, np.maximum(np.minimum((np.array(x) * 20 - 110), 50), 10), label = "Target")
+    plt.plot(mw, nrup, label='Actual', marker='x', linestyle='None')
+
+    plt.legend(loc='best')
+    plt.title('Magnitude vs Number of Realizations')
+    plt.ylabel('Number of Realizations')
+    plt.xlabel('Magnitude')
     plt.show()
     plt.close()
 
@@ -57,5 +89,7 @@ args = parser.parse_args()
 
 assert os.path.isdir(args.nhm_srf_dir)
 
+srf_dirs = glob(os.path.join(os.path.abspath(args.nhm_srf_dir), '*', 'Srf'))
 info_files = glob(os.path.join(os.path.abspath(args.nhm_srf_dir), '*', 'Srf', '*.info'))
 test_mw_vs_area(info_files)
+test_mw_vs_nrup(srf_dirs)
