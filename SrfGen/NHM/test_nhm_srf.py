63fc7e90fe98067f3c74d6bf7ecf63575d22869b
Viktor Polak <viktor.polak@canterbury.ac.nz>
2018-10-08 15:32:35 +1300

    area nhm vs area srf

--- SrfGen/NHM/test_nhm_srf.py
+++ SrfGen/NHM/test_nhm_srf.py
@@ -16,7 +16,7 @@ from scipy.stats import norm, kstest
 import numpy as np
 
 from createSRF import leonard
-from qcore.geo import ll_shift, ll_bearing
+from qcore.geo import ll_shift, ll_bearing, ll_dist
 from qcore import gmt
 
 def test_mw_vs_area(srf_infos, out_dir):
@@ -65,8 +65,7 @@ def test_mw_vs_area(srf_infos, out_dir):
 
     # part 2 : mw cs vs mw leonard
     fig = plt.figure(figsize = (8, 5), dpi = 150)
-    plt.plot(10**(y-4), y, label='Target (dip slip)')
-    plt.plot(10**(y-3.99), y, label='Target (strike slip)')
+    plt.plot(y, y, label='Target')
     if len(ds_mw):
         plt.plot(ds_mw, 4 + np.log10(ds_area), label='Actual (dip slip)', marker='x', linestyle='None')
     if len(ss_mw):
@@ -77,6 +76,61 @@ def test_mw_vs_area(srf_infos, out_dir):
     plt.ylabel('Mw based on area, based on Leonard')
     plt.savefig(os.path.join(out_dir, 'mw_vs_leonard'))
     plt.close()
+
+
+def test_area_vs_nhm(srf_dirs, nhm_file, out_dir):
+    area_srf_info = []
+    fault_names = []
+    for d in srf_dirs:
+        info = glob(os.path.join(d, '*.info'))[0]
+        with h5open(info, 'r') as i:
+            area_srf_info.append(sum(i.attrs['length']) * i.attrs['width'][0])
+            #print os.path.basename(os.path.dirname(d)), \
+            #      i.attrs['ndip'][0], i.attrs['width'][0], \
+            #      sum(i.attrs['nstrike']), sum(i.attrs['length']), \
+            #      i.attrs['nstrike'], i.attrs['length']
+        fault_names.append(os.path.basename(os.path.dirname(d)))
+
+    # grab area from nhm
+    area_nhm = np.zeros(len(fault_names))
+    with open(nhm_file, 'r') as n:
+        for _ in range(15):
+            n.readline()
+        nhm = n.readlines()
+        n_i = 0
+        while min(area_nhm) <= 0:
+            try:
+                name = nhm[n_i].strip()
+            except IndexError:
+                print('Could not find all SRFs in NHM.')
+                return
+            n_pt = int(nhm[n_i + 11])
+            if name in fault_names:
+                strace = [map(float, ll) for ll in map(str.split, map(str.strip, nhm[n_i + 12 : n_i + 12 + n_pt]))]
+                slen = sum([ll_dist(strace[i][0], strace[i][1], strace[i + 1][0], strace[i + 1][1]) for i in range(n_pt - 1)])
+                dbottom = float(nhm[n_i + 6].split()[0])
+                if dbottom <= 12:
+                    dbottom += 3
+                dwid = (dbottom - float(nhm[n_i + 7].split()[0])) / math.sin(math.radians(float(nhm[n_i + 3].split()[0])))
+                area_nhm[fault_names.index(name)] = slen * dwid
+            n_i += 13 + n_pt
+
+    # plot
+    fig = plt.figure(figsize = (8, 5), dpi = 150)
+    plt.plot([min(area_nhm), max(area_nhm)], [min(area_nhm), max(area_nhm)], label='Target')
+    plt.plot(area_nhm, area_srf_info, label='Actual', marker='x', linestyle='None')
+    plt.legend(loc='best')
+    plt.title('Area NHM vs Area SRF')
+    plt.xlabel('Area from NHM (km sq.)')
+    plt.ylabel('Area from SRF (km sq.)')
+    plt.savefig(os.path.join(out_dir, 'area_vs_nhm'))
+    plt.close()
+
+    # text
+    with open(os.path.join(out_dir, 'area_vs_nhm.txt'), 'w') as t:
+        t.write('name nhm srf\n')
+        for i in range(len(fault_names)):
+            t.write('%s %s %s\n' % (fault_names[i], area_nhm[i], area_srf_info[i]))
 
 def test_mw_vs_nhm(srf_dirs, nhm_file, out_dir):
     # part 3 : mw cs vs mw nhm
@@ -106,7 +160,8 @@ def test_mw_vs_nhm(srf_dirs, nhm_file, out_dir):
 
     # plot
     fig = plt.figure(figsize = (8, 5), dpi = 150)
-    plt.plot(mw_nhm, mw_srf_info)
+    plt.plot([min(mw_nhm), max(mw_nhm)], [min(mw_nhm), max(mw_nhm)], label='Target')
+    plt.plot(mw_nhm, mw_srf_info, label='Actual', marker='x', linestyle='None')
     plt.legend(loc='best')
     plt.title('Mw NHM vs Mw SRF info')
     plt.xlabel('Mw from NHM')
@@ -186,6 +241,7 @@ def nhm2corners(nhm_file, names):
     corners_sub = []
     corners_shal = []
     ftypes = {}
+    ex_names = []
     with open(nhm_file, 'r') as n:
         for _ in range(15):
             n.readline()
@@ -198,6 +254,7 @@ def nhm2corners(nhm_file, names):
         dbottom = float(nhm[n_i + 6].split()[0])
         dtop = float(nhm[n_i + 7].split()[0])
         if n not in names:
+            ex_names.append(n)
             pwid = (dbottom - dtop + 3 * (dbottom >= 12)) \
                    / math.tan(math.radians(float(nhm[n_i + 3].split()[0])))
             ftype -= 2
@@ -218,7 +275,7 @@ def nhm2corners(nhm_file, names):
         ftypes[n] = ftype
         n_i += 13 + n_pt
 
-    return ftypes, corners_shal, corners_sub
+    return ftypes, corners_shal, corners_sub, ex_names
 
 def test_spacial_srf(srf_dirs, nhm_file, out_dir):
     tmp = mkdtemp()
@@ -266,7 +323,7 @@ def test_spacial_srf(srf_dirs, nhm_file, out_dir):
 
     # nhm faults
     faults = map(os.path.basename, map(os.path.dirname, srf_dirs))
-    ftypes, nhm_cnrs_shal, nhm_cnrs_sub = nhm2corners(nhm_file, faults)
+    ftypes, nhm_cnrs_shal, nhm_cnrs_sub, ex_names = nhm2corners(nhm_file, faults)
     for x in p, q:
         x.path('\n>\n'.join(map(corners2gmt, nhm_cnrs_shal)), is_file=False, \
                colour='40/40/40', split='-', fill='%s@80' % (cpt_colours[1]), \
@@ -295,9 +352,13 @@ def test_spacial_srf(srf_dirs, nhm_file, out_dir):
         x.png(dpi=300, background='white', out_dir=out_dir)
     rmtree(tmp)
 
+    # save excluded fault names
+    with open(os.path.join(out_dir, 'excluded_faults.txt'), 'w') as e:
+        e.write('\n'.join(ex_names))
+        e.write('\n')
+
 
 def test_hypo_distribution(srf_dirs, out_dir):
-    srf_dirs=srf_dirs[:20]
     if os.path.isdir(out_dir):
         rmtree(out_dir)
     os.makedirs(out_dir)
@@ -311,6 +372,10 @@ def test_hypo_distribution(srf_dirs, out_dir):
     w_x.sort()
     w_y = np.arange(w_x.size) / (w_x.size - 1.0)
     w_call = lambda x : w_y[[np.argmin(np.abs(v-w_x)) for v in x]]
+
+    # DEBUG
+    #np.savetxt(os.path.join(out_dir, 'normal.txt'), np.dstack((n_x, n_y))[0])
+    #np.savetxt(os.path.join(out_dir, 'weibull.txt'), np.dstack((w_x, w_y))[0])
 
     # kstest
     mw = []
@@ -334,10 +399,7 @@ def test_hypo_distribution(srf_dirs, out_dir):
         y = np.arange(shypo.size) / (shypo.size - 1.0)
 
         # kstest
-        #print shypo
-        #print n_call(shypo)
         p_s.append(kstest(shypo, n_call).pvalue)
-        #print kstest(shypo, n_call)
         p_d.append(kstest(dhypo, w_call).pvalue)
 
         fig = plt.figure(figsize = (8, 5), dpi = 150)
@@ -351,6 +413,8 @@ def test_hypo_distribution(srf_dirs, out_dir):
                  linestyle='None', color='orange')
         plt.plot(shypo[1:-1], y[1:-1], marker='.', markersize=2, \
                  linestyle='None', color='green')
+        #np.savetxt('%s_shypo.txt' % (os.path.join(out_dir, n)), np.dstack((shypo[1:-1], y[1:-1]))[0])
+        #np.savetxt('%s_dhypo.txt' % (os.path.join(out_dir, n)), np.dstack((dhypo[1:-1], y[1:-1]))[0])
 
         plt.legend(loc='best')
         plt.title('%s Hypocentre Location Distribution (%d realizations)' % (n, shypo.size))
@@ -363,6 +427,7 @@ def test_hypo_distribution(srf_dirs, out_dir):
 
     # overall plot
     fig = plt.figure(figsize = (8, 5), dpi = 150)
+    plt.plot([min(mw), max(mw)], [0.05, 0.05])
     plt.plot(mw, p_s, label="p (along strike)", marker='x', linestyle='None')
     plt.plot(mw, p_d, label="p (along dip)", marker='x', linestyle='None')
 
@@ -370,7 +435,8 @@ def test_hypo_distribution(srf_dirs, out_dir):
     plt.title('Hypocentre Location Distribution (%d simulations)' % (len(mw)))
     plt.ylabel('p')
     plt.xlabel('magnitude')
-    plt.gca().set_ylim([0, 1])
+    plt.gca().set_yscale('log')
+    #plt.gca().set_ylim([0, 1])
     plt.savefig(os.path.join(out_dir, 'all'))
     plt.close()
 
@@ -477,20 +543,22 @@ if not os.path.isdir(args.out_dir):
 if args.srf_dir is not None:
     srf_dirs = glob(os.path.join(os.path.abspath(args.srf_dir), '*', 'Srf'))
     srf_faults = map(os.path.basename, map(os.path.dirname, srf_dirs))
-    info_files = glob(os.path.join(os.path.abspath(args.srf_dir), '*', 'Srf', '*.info'))
+    #info_files = glob(os.path.join(os.path.abspath(args.srf_dir), '*', 'Srf', '*.info'))
 
-    test_mw_vs_area(info_files, args.out_dir)
-    test_mw_vs_nhm(srf_dirs, nhm_file, out_dir)
-    test_mw_vs_nrup(srf_dirs, args.out_dir)
-    test_seismogenic_depth(srf_dirs, args.nhm_file, args.out_dir)
-    test_spacial_srf(srf_dirs, args.nhm_file, args.out_dir)
+    #test_mw_vs_area(info_files, args.out_dir)
+    test_area_vs_nhm(srf_dirs, args.nhm_file, args.out_dir)
+    #test_mw_vs_nhm(srf_dirs, args.nhm_file, args.out_dir)
+    #test_mw_vs_nrup(srf_dirs, args.out_dir)
+    #test_seismogenic_depth(srf_dirs, args.nhm_file, args.out_dir)
+    #test_spacial_srf(srf_dirs, args.nhm_file, args.out_dir)
     #if args.nproc == 1:
-    test_hypo_distribution(srf_dirs, os.path.join(args.out_dir, 'distributions'))
+    #test_hypo_distribution(srf_dirs, os.path.join(args.out_dir, 'distributions'))
     #else:
     #    p = Pool(args.nproc)
     #    p.map(test_hypo_distribution, srf_dirs)
-    if args.selection_file is not None:
-        test_selection(args.selection_file, names, 'SRF')
+    # TODO: where does names come from??
+    #if args.selection_file is not None:
+    #    test_selection(args.selection_file, names, 'SRF')
 if args.vm_dir is not None and False:
     vm_json = glob(os.path.join(args.vm_dir, '*', 'params_vel.json'))
     vm_dirs = map(os.path.dirname, vm_json)
