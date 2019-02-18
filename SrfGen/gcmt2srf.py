91fdb48f6c0471dbb9308c9cac5e5f90569e65c8
jpa198 <james.paterson@canterbury.ac.nz>
2019-02-18 17:11:08 +1300

    Fixed issue where means were one level lower than expected in the additional options dictionary

--- SrfGen/gcmt2srf.py
+++ SrfGen/gcmt2srf.py
@@ -20,6 +20,8 @@ def run_create_srf(args, t, vs, rho, n_sims):
     if args.uncertainty_file:
         with open(args.add_opts_file) as ao_f:
             add_opts = yaml.load(ao_f)
+        for opt in add_opts.keys():
+            add_opts.update({opt, add_opts[opt]})
         create_ps_realisation(args.out_dir, str(t.pid), t.lat, t.lon, t.depth, t.mag, mom, t.strike, t.rake, t.dip,
                               args.uncertainty_file, n_realisations=n_sims, additional_options=add_opts,
                               dt=args.dt, vs=vs, rho=rho, silent=True)
