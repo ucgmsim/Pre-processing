6addcedfd9fb67a3e28e8cfb7ef0934b9eb96c61
jpa198 <james.paterson@canterbury.ac.nz>
2019-10-04 11:03:34 +1300

    Blacked Removed unneeded file

--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="Pre-processing",
     version="19.9.1",
-    packages=find_namespace_packages(include=['srf_generation.*']),
+    packages=find_namespace_packages(include=["srf_generation.*"]),
     url="https://github.com/ucgmsim/Pre-processing",
     description="Srf generation code",
     install_requires=["numpy>=1.14.3", "scipy>=1.1.0"],
-    scripts=[
-        "srf_generation/source_parameter_generation/gcmt_to_srfgen.py",
-    ],
+    scripts=["srf_generation/source_parameter_generation/gcmt_to_srfgen.py"],
 )
