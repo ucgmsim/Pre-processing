98c8c8d254f9d7dbd5edaba0fc4d8dbdd966b35b
jpa198 <james.paterson@canterbury.ac.nz>
2019-10-30 16:52:05 +1300

    Create new multi source params srf generation script. Uses the core of the single generation script with a worker pool to generate the srfs.

---
+++ srf_generation/input_file_generation/generate_srf_from_srfgenparams.py
@@ -0,0 +1,36 @@
+import argparse
+import glob
+from multiprocessing.pool import Pool
+from os import path
+import pandas as pd
+
+from qcore import simulation_structure
+
+from srf_generation.input_file_generation.srfgenparams_to_srf import create_ps_srf
+
+
+def process_srfgenparams_file(cybershake_root, srfgenparams_file):
+    rel_df: pd.DataFrame = pd.read_csv(srfgenparams_file)
+    realisation = rel_df.to_dict()
+    if realisation["type"] == 1:
+        create_ps_srf(cs_root=cybershake_root, **realisation)
+
+
+def load_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cybershake_root", type=path.abspath, default=path.abspath(""))
+    parser.add_argument("-n", "--n_processes", default=1, type=int)
+    return parser.parse_args()
+
+
+def main():
+    args = load_args()
+
+    srfgenparams_path = simulation_structure.get_srf_path(args.cybershake_root, "*").replace(".srf", ".csv")
+    srfgenparams_files = glob.glob(srfgenparams_path)
+
+    worker_pool = Pool(args.n_processes)
+    worker_pool.map(
+        lambda filename: process_srfgenparams_file(args.cybershake_root, filename),
+        srfgenparams_files
+    )
