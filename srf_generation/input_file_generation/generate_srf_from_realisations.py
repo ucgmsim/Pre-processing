9cec9dbdf85919787126db38255339f03ace1098
jpa198 <james.paterson@canterbury.ac.nz>
2019-12-05 16:34:11 +1300

    Removed common info file generation as there is no longer a special case required for median info files

--- srf_generation/input_file_generation/generate_srf_from_realisations.py
+++ srf_generation/input_file_generation/generate_srf_from_realisations.py
@@ -1,9 +1,7 @@
 import argparse
 import glob
-from logging import Logger
 from multiprocessing.pool import Pool
 from os import path
-from typing import Dict
 
 import pandas as pd
 
@@ -12,7 +10,6 @@ from qcore.qclogging import NOPRINTCRITICAL, add_general_file_handler
 from srf_generation.input_file_generation.realisation_to_srf import (
     create_ps_srf,
     generate_sim_params_yaml,
-    create_info_file,
     create_ps_ff_srf,
 )
 
@@ -60,60 +57,6 @@ def process_realisation_file(
     generate_sim_params_yaml(sim_params_file, realisation, realisation_logger)
 
 
-def process_common_realisation_file(
-    cybershake_root, realisation_file, logger: Logger = qclogging.get_basic_logger()
-):
-    rel_df: pd.DataFrame = pd.read_csv(realisation_file, dtype={"name": str})
-    realisation: Dict = rel_df.to_dict(orient="records")[0]
-    srf_file = simulation_structure.get_srf_path(
-        cybershake_root,
-        simulation_structure.get_realisation_name(realisation["name"], 1),
-    )
-    info_filename = realisation_file.replace(".csv", ".info")
-    if realisation["type"] == 1:
-        create_info_file(
-            srf_file=srf_file,
-            srf_type=1,
-            mag=realisation.get("magnitude"),
-            rake=realisation.get("rake"),
-            dt=realisation.get("dt", 0.005),
-            vs=realisation.get("vs", 3.2),
-            rho=realisation.get("rho", 2.44),
-            file_name=info_filename,
-            lon=realisation.get("longitude"),
-            lat=realisation.get("latitude"),
-            centroid_depth=realisation.get("depth"),
-        )
-    elif realisation["type"] == 2:
-        create_info_file(
-            srf_file,
-            2,
-            realisation.get("magnitude"),
-            realisation.get("rake"),
-            realisation.get("dt", 0.005),
-            centroid_depth=realisation.get("depth"),
-            lon=None,
-            lat=None,
-            tect_type=None,
-            dip_dir=None,
-            mwsr=realisation.get("mwsr"),
-            shypo=realisation.get("shypo") + 0.5 * realisation.get("flen"),
-            dhypo=realisation.get("dhypo"),
-            vm=realisation.get("vel_mod_1d"),
-            logger=logger,
-        )
-    else:
-        raise ValueError(
-            f"Type {realisation['type']} faults are not currently supported. "
-            f"Contact the software team if you believe this is an error."
-        )
-
-    sim_params_file = simulation_structure.get_source_params_path(
-        cybershake_root, realisation["name"]
-    )
-    generate_sim_params_yaml(sim_params_file, realisation)
-
-
 def load_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -147,16 +90,6 @@ def main():
             (args.cybershake_root, filename, primary_logger.name)
             for filename in realisation_files
         ],
-    )
-
-    realisations_path = path.join(
-        simulation_structure.get_sources_dir(args.cybershake_root), "*", "*.csv"
-    )
-    realisation_files = glob.glob(realisations_path)
-
-    worker_pool.starmap(
-        process_common_realisation_file,
-        [(args.cybershake_root, filename) for filename in realisation_files],
     )
 
 
