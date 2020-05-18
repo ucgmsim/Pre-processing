Added magnitude to area Skarlatoudis calculation Added alpha_rough to params, and skarlatoudis scaling to type 2 srf generation Fixed minor bugs Sorted realisation files so they can act as a sort of progress meter Added check pointing feature in comments as it was used in testing and may as well be available in production. Should probably have an argument flag attached.

srf_generation/Fault.py
@@ -344,7 +344,7 @@ class Type2(FiniteFault):
         self._dbottom = (
             self._depth + np.sin(np.radians(self._dip)) * self.width / 2 + shift
         )
-        if self._dbottom > NHM_SEISMOGENIC_DEPTH:
+        if self.magnitude_scaling_relation == MagnitudeScalingRelations.LEONARD2014 and self._dbottom > NHM_SEISMOGENIC_DEPTH:
             self._dbottom += LEONARD_SEISMOGENIC_DEPTH_DIFFERENCE
 
         self.ny = int(round(self.width / self.dwid))
@@ -471,7 +471,7 @@ class Type2(FiniteFault):
 
 class Type3(FiniteFault):
     def __init__(
-        self, name, magnitude, lon1, lat1, lon2, lat2, dip, rake, dtop, dbottom, dip_dir
+        self, name, magnitude, lon1, lat1, lon2, lat2, dip, rake, dtop, dbottom, dip_dir, tectonic_type
     ):
         self._trace = ((lon1, lat1), (lon2, lat2))
         self._dtop = dtop
@@ -481,12 +481,15 @@ class Type3(FiniteFault):
         self._length = round_subfault_size(
             geo.ll_dist(lon1, lat1, lon2, lat2), magnitude
         )
-        if dbottom >= NHM_SEISMOGENIC_DEPTH:
-            raw_fwid = (dbottom - dtop + LEONARD_SEISMOGENIC_DEPTH_DIFFERENCE) / np.sin(
-                np.radians(dip)
-            )
+
+        if tectonic_type == "SUBDUCTION_INTERFACE":
+            self.mwsr = MagnitudeScalingRelations.SKARLATOUDIS2016
+
         else:
-            raw_fwid = (dbottom - dtop) / np.sin(np.radians(dip))
+            self.mwsr = MagnitudeScalingRelations.LEONARD2014
+            self._dbottom += 3
+
+        raw_fwid = (self._dbottom - dtop) / np.sin(np.radians(dip))
         self._width = round_subfault_size(raw_fwid, magnitude)
 
         strike = geo.ll_bearing(lon1, lat1, lon2, lat2)
@@ -540,6 +543,7 @@ class Type4(MultiPlaneFault):
 
         else:
             self.mwsr = MagnitudeScalingRelations.LEONARD2014
+            self._dbottom += 3
 
         dummy_plane = Type3(
             nhm_data.name,
@@ -551,6 +555,7 @@ class Type4(MultiPlaneFault):
             nhm_data.dtop,
             nhm_data.dbottom,
             nhm_data.dip_dir,
+            nhm_data.tectonic_type,
         )
 
         length = sum(
@@ -580,6 +585,7 @@ class Type4(MultiPlaneFault):
                     nhm_data.dtop,
                     nhm_data.dbottom,
                     nhm_data.dip_dir,
+                    nhm_data.tectonic_type,
                 )
             )
 
@@ -632,7 +638,7 @@ class Type4(MultiPlaneFault):
             "dip": self._dip,
             "dtop": self._dtop,
             "dbottom": self._dbottom,
-            "length": self._length,
+            "length": self.length,
             "plane_count": self._n_planes,
             "slip_rate": self._slip_rate,
             "dip_dir": self._dip_dir,

srf_generation/input_file_generation/realisation_to_srf.py
@@ -313,6 +313,7 @@ def create_ps_ff_srf(
     seed = parameter_dictionary.pop("srfgen_seed")
     slip_cov = parameter_dictionary.pop("slip_cov", None)
     rough = parameter_dictionary.pop("rough", None)
+    tect_type = parameter_dictionary.pop("tect_type", None)
 
     mwsr = MagnitudeScalingRelations(parameter_dictionary.pop("mwsr"))
 
@@ -373,6 +374,7 @@ def create_ps_ff_srf(
         slip_cov=slip_cov,
         rough=rough,
         logger=logger,
+        tect_type=tect_type,
     )
 
     if stoch_file is None:
@@ -403,10 +405,11 @@ def create_multi_plane_srf(
     realisation_file: str,
     parameter_dictionary: Dict[str, Any],
     stoch_file: Union[None, str] = None,
-    logger: Logger = qclogging.get_basic_logger,
+    logger: Logger = qclogging.get_basic_logger(),
 ):
     name = parameter_dictionary.get("name")
-    logger.info(f"Generating srf for realisation {name}")
+    rel_logger = qclogging.get_realisation_logger(logger, name)
+    rel_logger.info(f"Generating srf for realisation {name}")
 
     # pops
     magnitude = parameter_dictionary.pop("magnitude")
@@ -468,14 +471,14 @@ def create_multi_plane_srf(
         )
 
     gsf_file = NamedTemporaryFile(mode="w", delete=False)
-    logger.debug(f"Gsf will be saved to the temporary file {gsf_file.name}")
+    rel_logger.debug(f"Gsf will be saved to the temporary file {gsf_file.name}")
     srf_file = realisation_file.replace(".csv", ".srf")
-    logger.debug(f"Srf will be saved to {srf_file}")
+    rel_logger.debug(f"Srf will be saved to {srf_file}")
     corners_file = realisation_file.replace(".csv", ".corners")
-    logger.debug(f"The corners file will be saved to {corners_file}")
+    rel_logger.debug(f"The corners file will be saved to {corners_file}")
 
     with NamedTemporaryFile(mode="w", delete=False) as gsfp:
-        logger.debug("Saving segments file to {}".format(gsfp.name))
+        rel_logger.debug("Saving segments file to {}".format(gsfp.name))
         gsfp.write("{}\n".format(plane_count))
         for f in range(plane_count):
             gsfp.write(
@@ -503,15 +506,15 @@ def create_multi_plane_srf(
     if dip_dir is not None:
         cmd.append("dipdir={}".format(dip_dir))
 
-    logger.info("Calling fault_seg2gsf_dipdir with command {}".format(" ".join(cmd)))
+    rel_logger.info("Calling fault_seg2gsf_dipdir with command {}".format(" ".join(cmd)))
     gexec = run(cmd)
-    logger.debug(f"{fault_seg_bin} finished running with stderr: {gexec.stderr}")
+    rel_logger.debug(f"{fault_seg_bin} finished running with stderr: {gexec.stderr}")
 
-    remove(gsfp.name)
-    logger.debug("Removed segments file")
+    # remove(gsfp.name)
+    # rel_logger.debug("Removed segments file")
 
     if int(plane_count > 1):
-        logger.debug("Multiple segments detected. Generating xseg argument")
+        rel_logger.debug("Multiple segments detected. Generating xseg argument")
         flen_array = np.asarray(flen)
         xseg = ",".join(map(str, flen_array.cumsum() - flen_array / 2))
     else:
@@ -533,16 +536,17 @@ def create_multi_plane_srf(
         rvfac=rvfac,
         rough=rough,
         xseg=xseg,
-        logger=logger,
+        logger=rel_logger,
+        tect_type=tect_type,
     )
 
-    logger.info("srf generated, creating stoch")
+    rel_logger.info("srf generated, creating stoch")
 
     if stoch_file is None:
         stoch_file = realisation_file.replace(".csv", ".stoch")
     create_stoch(stoch_file, srf_file, single_segment=(plane_count == 1), logger=logger)
 
-    logger.info("stoch created, making info")
+    rel_logger.info("stoch created, making info")
 
     # save INFO
     create_info_file(
@@ -556,10 +560,10 @@ def create_multi_plane_srf(
         shypo=[shypo + 0.5 * flen[0]],
         dhypo=dhypo,
         vm=vel_mod_1d,
-        logger=logger,
+        logger=rel_logger,
     )
 
-    logger.info("info made, returning")
+    rel_logger.info("info made, returning")
 
     # path to resulting SRF
     return srf_file