53b9a8a9f53c285ac861cb255fb5bab1aff0fd2d
Sung Eun Bae <sung.bae@canterbury.ac.nz>
2020-11-25 12:54:27 +1300

    Updating myJenkins.sh to overcome env variable access issue

--- myJenkins.sh
+++ myJenkins.sh
@@ -1,12 +1,11 @@
 source /var/lib/jenkins/py3env/bin/activate
-cd ${env.WORKSPACE}
 pip install -r requirements.txt
 
-mkdir -p /tmp/${env.EXECUTOR_NUMBER}
-cd /tmp/${env.EXECUTOR_NUMBER}
+mkdir -p /tmp/${id}
+cd /tmp/${id}
 rm -rf qcore
 git clone https://github.com/ucgmsim/qcore.git
 pip install --no-deps ./qcore/
-cd ${env.WORKSPACE}
+cd -
 pytest --black --ignore=geoNet --ignore=NonUniformGrid --ignore=RegionalSeismicityTectonics --ignore=SrfGen/NHM/deprecated --ignore=test
-if [ ! -z ${env.EXECUTOR_NUMBER} ]; then rm -rf /tmp/${env.EXECUTOR_NUMBER};echo '/tmp/${env.EXECUTOR_NUMBER} deleted';else echo "${env.EXECUTOR_NUMBER} is not set";fi;
+if [ ! -z ${id} ]; then rm -rf /tmp/${id};echo '/tmp/${id} deleted';else echo "${id} is not set";fi;
