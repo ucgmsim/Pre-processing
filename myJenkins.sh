03e5100b5ea84ec2229f168951bc2f31c355e710
Sung Eun Bae <sung.bae@canterbury.ac.nz>
2020-11-25 13:00:23 +1300

    Print each step

--- myJenkins.sh
+++ myJenkins.sh
@@ -1,6 +1,6 @@
 source /var/lib/jenkins/py3env/bin/activate
+echo "DEPENDENCY INSTALLED"
 pip install -r requirements.txt
-
 mkdir -p /tmp/${id}
 cd /tmp/${id}
 rm -rf qcore
@@ -8,5 +8,7 @@ git clone https://github.com/ucgmsim/qcore.git
 pip install --no-deps ./qcore/
 cd -
 echo `pwd`
+echo "TEST BEGINS"
 pytest --black --ignore=geoNet --ignore=NonUniformGrid --ignore=RegionalSeismicityTectonics --ignore=SrfGen/NHM/deprecated --ignore=test
+echo "CLEAN UP"
 if [ ! -z ${id} ]; then rm -rf /tmp/${id};echo '/tmp/${id} deleted';else echo "${id} is not set";fi;
