be6669897bc2bdfa3fc0736ea66b267f471376d2
Sung Eun Bae <sung.bae@canterbury.ac.nz>
2020-11-25 13:21:07 +1300

    Updated README.md and myJenkins.sh to summarize the changewq

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 # Pre-processing
 
 # Jenkins
-Testing if a Pull-request triggers the Jenkins job, and if the same Jenkins job can be triggered manually
-To do: When manually triggered, ghprbActualCommit is empty, and regarded as null
-Replaced Jenkinsfile with myJenkins.sh
+- Jenkinsfile replaced with myJenkins.sh
+- Appears to be triggered by a pull request ok
+- Can be manually triggered
+- Integrated with workspace cleanup plugin (Had to be a freestyle project - ie. Jenkinsfile-based pipeline not working)
+- TO DO: If testing fails, does it report as a fail?
 
--- myJenkins.sh
+++ myJenkins.sh
@@ -1,5 +1,5 @@
 source /var/lib/jenkins/py3env/bin/activate
-echo "DEPENDENCY INSTALLED"
+echo "============================ DEPENDENCY INSTALLED ============================"
 pip install -r requirements.txt
 mkdir -p /tmp/${id}
 cd /tmp/${id}
@@ -8,7 +8,7 @@ git clone https://github.com/ucgmsim/qcore.git
 pip install --no-deps ./qcore/
 cd -
 echo `pwd`
-echo "TEST BEGINS"
+echo "================================= TEST BEGINS ================================="
 pytest --black --ignore=geoNet --ignore=NonUniformGrid --ignore=RegionalSeismicityTectonics --ignore=SrfGen/NHM/deprecated --ignore=test
-echo "CLEAN UP"
+echo "=================================== CLEAN UP =================================="
 if [ ! -z ${id} ]; then rm -rf /tmp/${id};echo '/tmp/${id} deleted';else echo "${id} is not set";fi;
