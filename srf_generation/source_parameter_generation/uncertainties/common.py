f9e7fdb8307a60dd32c3a7669fe4f4ae87894eb4
James <james.paterson@canterbury.ac.nz>
2022-12-13 11:31:13 +1300

    Change bb_sim to only need one argument Add entry to srf_gen so site amp uncertainty can be set by version files

--- srf_generation/source_parameter_generation/uncertainties/common.py
+++ srf_generation/source_parameter_generation/uncertainties/common.py
@@ -170,7 +170,7 @@ HF_RUN_PARAMS = [
 
 LF_RUN_PARAMS = ["qsfrac", "qpfrac", "qpqs_factor"]
 
-BB_RUN_PARAMS = ["flo", "fmin", "fmidbot", "lfvsref"]
+BB_RUN_PARAMS = ["flo", "fmin", "fmidbot", "lfvsref", "site-amp-uncertainty"]
 
 RUN_TIME_PARAMS = HF_RUN_PARAMS + LF_RUN_PARAMS + BB_RUN_PARAMS
 LEONARD_SEISMOGENIC_DEPTH_DIFFERENCE = 3
